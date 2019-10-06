1fccf8152a5650fb81e6105dc3e075229fa69cef
ArturasLT578 <arturas.paluskinas@gmail.com>
2019-10-06 12:30:15 +0300

    hello_world2

---
+++ src/new_main.java
@@ -0,0 +1,14 @@
+public class new_main {
+    public static void main(String[] args) {
+        String myName;
+        String customersName;
+
+        myName = "hello_world2";
+        customersName = "customer_name";
+
+                System.out.println(myName+" "+customersName
+                );
+
+
+    }
+}
